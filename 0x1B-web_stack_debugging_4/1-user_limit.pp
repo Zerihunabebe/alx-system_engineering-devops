8d209539784769bfde7ce053d2315f5b7500b367
root <abebezerihun4@gmail.com>
2022-11-29 05:11:32 +0000

    Updated

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,8 @@
-#Enable the user holberton to login and open file without errors
+#Enable the user holberton to login and open files without errors
 
 # Increase hard file limit for holberton user
-exec { 'increase-hard-file-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limit.conf'
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
   path    => '/usr/local/bin/:/bin/'
 }
 
